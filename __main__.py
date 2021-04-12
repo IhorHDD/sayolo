1f4106b73e22ba8300324ad98f762b0807db7c40
Ihor Kanivets <ihor.kanivets@dowjones.com>
2021-04-12 17:30:53 +0300

    feat: [Ticket-1] EKS creation with Pulumi

---
+++ __main__.py
@@ -0,0 +1,161 @@
+"""An AWS Python Pulumi program"""
+
+import pulumi
+import pulumi_aws as aws
+import pulumi_docker as docker
+import base64
+from pulumi_kubernetes.apps.v1 import Deployment
+from pulumi_kubernetes.core.v1 import Service
+import pulumi_eks as eks
+
+# Create an EKS cluster.
+cluster = eks.Cluster(
+    "cluster",
+    instance_type="c5.xlarge",
+    desired_capacity=1,
+    min_size=1,
+    max_size=1,
+)
+
+
+sayolo = aws.ecr.Repository("sayolo",
+    image_scanning_configuration=aws.ecr.RepositoryImageScanningConfigurationArgs(
+        scan_on_push=True,
+    ),
+    image_tag_mutability="MUTABLE")
+
+token = aws.ecr.get_authorization_token()
+
+# def getRegistryInfo(registry_id):
+#     creds = aws.ecr.get_credentials(registry_id=sayolo.registry_id)
+#     decoded = base64.b64decode(creds.authorization_token).decode()
+#     parts = decoded.split(':')
+#     if len(parts) != 2:
+#         raise Exception("Invalid credentials")
+#     return docker.ImageRegistry(creds.proxy_endpoint, parts[0], parts[1])
+# image_name = sayolo.repository_url
+# registry_info = sayolo.registry_id.apply(getRegistryInfo)
+
+# Get registry info (creds and endpoint).
+image_name = sayolo.repository_url
+registry_info = None # use ECR credentials helper.
+
+
+def getRegistryInfo(rid):
+    creds = aws.ecr.get_credentials(registry_id=rid)
+    decoded = base64.b64decode(creds.authorization_token).decode()
+    parts = decoded.split(':')
+    if len(parts) != 2:
+        raise Exception("Invalid credentials")
+    return docker.ImageRegistry(creds.proxy_endpoint, parts[0], parts[1])
+image_name = sayolo.repository_url
+registry_info = sayolo.registry_id.apply(getRegistryInfo)
+
+
+
+# # [Registry configuration as shown above ...]
+
+# Build and publish the container image.
+image = docker.Image('sayolo_build',
+    build='.',
+    image_name=image_name,
+    registry=registry_info,
+)
+
+# Export the base and specific version image name.
+pulumi.export('baseImageName', image.base_image_name)
+pulumi.export('fullImageName', image.image_name)
+
+
+sayolo = aws.ecr.Repository("sayolo",
+    image_scanning_configuration=aws.ecr.RepositoryImageScanningConfigurationArgs(
+        scan_on_push=True,
+    ),
+    image_tag_mutability="MUTABLE")
+
+token = aws.ecr.get_authorization_token()
+
+# def getRegistryInfo(registry_id):
+#     creds = aws.ecr.get_credentials(registry_id=sayolo.registry_id)
+#     decoded = base64.b64decode(creds.authorization_token).decode()
+#     parts = decoded.split(':')
+#     if len(parts) != 2:
+#         raise Exception("Invalid credentials")
+#     return docker.ImageRegistry(creds.proxy_endpoint, parts[0], parts[1])
+# image_name = sayolo.repository_url
+# registry_info = sayolo.registry_id.apply(getRegistryInfo)
+
+# # Get registry info (creds and endpoint).
+# image_name = sayolo.repository_url
+# registry_info = None # use ECR credentials helper.
+
+
+# def getRegistryInfo(rid):
+#     creds = aws.ecr.get_credentials(registry_id=rid)
+#     decoded = base64.b64decode(creds.authorization_token).decode()
+#     parts = decoded.split(':')
+#     if len(parts) != 2:
+#         raise Exception("Invalid credentials")
+#     return docker.ImageRegistry(creds.proxy_endpoint, parts[0], parts[1])
+# image_name = sayolo.repository_url
+# registry_info = sayolo.registry_id.apply(getRegistryInfo)
+
+
+# # Build and publish the container image.
+# image = docker.Image('sayolo_build',
+#     build='.',
+#     image_name=image_name,
+#     registry=registry_info,
+# )
+
+# Export the base and specific version image name.
+pulumi.export('baseImageName', image.base_image_name)
+pulumi.export('fullImageName', image.image_name)
+
+
+# Export the cluster's kubeconfig.
+pulumi.export("kubeconfig", cluster.kubeconfig)
+
+
+# Minikube does not implement services of type `LoadBalancer`; require the user to specify if we're
+# running on minikube, and if so, create only services of type ClusterIP.
+config = pulumi.Config()
+is_minikube = cluster.kubeconfig
+#.require_bool("isMinikube")
+is_minikube = False
+app_name = "nginx"
+app_labels = { "app": app_name }
+
+deployment = Deployment(
+    "nginx",
+    spec={
+        "selector": { "match_labels": app_labels },
+        "replicas": 4,
+        "template": {
+            "metadata": { "labels": app_labels },
+            "spec": { "containers": [{ "name": app_name, "image": "nginx"}] }
+        }
+    })
+
+# Allocate an IP to the Deployment.
+frontend = Service(
+    app_name,
+    metadata={
+        "labels": deployment.spec["template"]["metadata"]["labels"],
+    },
+    spec={
+        "type": "ClusterIP" if i else "LoadBalancer",
+        "ports": [{ "port": 80, "target_port": 80, "protocol": "TCP" }],
+        "selector": app_labels,
+    })
+
+# When "done", this will print the public IP.
+result = None
+if is_minikube:
+    result = frontend.spec.apply(lambda v: v["cluster_ip"] if "cluster_ip" in v else None)
+else:
+    ingress = frontend.status.apply(lambda v: v["load_balancer"]["ingress"][0] if "load_balancer" in v else None)
+    if ingress is not None:
+        result = ingress.apply(lambda v: v["ip"] if "ip" in v else v["hostname"])
+
+pulumi.export("ip", result)
